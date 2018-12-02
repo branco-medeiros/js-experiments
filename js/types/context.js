f2c7ecbcefebeccefe014184bd96fdb8e8112b5e
branco-medeiros <branco-medeiros@users.noreply.github.com>
2018-12-02 15:51:01 -0300

    moved Context.Result to its own file

--- js/types/context.js
+++ js/types/context.js
@@ -1,53 +1,6 @@
-define(["types/track", "types/iterator"],
-function(Track, Iterator){
+define(["types/track", "types/iterator", "types/result"],
+function(Track, Iterator, Result){
 
-	class Result {
-		constructor(rule, start, end){
-			this.rule = rule
-			this.values = []
-			this.start = start || 0
-			this.end = end
-			this.isResult = true
-		}
-
-		get first(){
-			return this.values[0]
-		}
-
-		get last(){
-			return this.values[this.values.length-1]
-		}
-
-		at(index){
-			return this.values[index]
-		}
-
-		get count(){
-			return this.values.length
-		}
-
-		display(options, ilevel){
-
-			options = options || {}
-			ilevel = ~~ilevel
-			var content = this.values.map((v) => v.display(options, ilevel + 1)).join(" ")
-			return "(" +
-				(options.showILevel? ilevel + ":" : "") +
-				((this.rule && this.rule.name) || "N/R") +
-				(this.start == null? ""
-					:(" " + this.start + ":" + (this.end == null? "?": this.end))
-				) +
-				(content == null || !content.length? "" : (" " + content)) +
-			")"
-		}
-		toString(){
-			return this.display()
-		}
-
-		static create(rule, start, end){
-			return new Result(rule, start, end)
-		}
-	}
 
 	class Context extends Iterator{
 
@@ -238,10 +191,6 @@ function(Track, Iterator){
 			return new Context(value)
 		}
 
-	}
-
-	Context.types = {
-		Result: Result
 	}
 
 	return Context
